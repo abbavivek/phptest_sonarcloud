8ba7daeb506dcd805879f655e3cad8216fdff1d0
Abba Vivek <abbaviveks@gmail.com>
2023-07-28 15:31:55 +0800

    add files

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+// tests/CalculatorTest.php
+
+use PHPUnit\Framework\TestCase;
+
+// Include the Calculator class
+require_once __DIR__ . '/../src/Calculator.php';
+
+class CalculatorTest extends TestCase {
+    public function testAdd() {
+        $calculator = new Calculator();
+        $result = $calculator->add(3, 5);
+        $this->assertEquals(8, $result);
+    }
+
+    public function testSubtract() {
+        $calculator = new Calculator();
+        $result = $calculator->subtract(10, 3);
+        $this->assertEquals(7, $result);
+    }
+}
