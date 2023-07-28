52dbb652db1458bf1b23b9a23a10910230ec2783
Abba Vivek <abbaviveks@gmail.com>
2023-07-28 17:00:32 +0800

    Update Calculator.php

--- src/Calculator.php
+++ src/Calculator.php
@@ -17,4 +17,19 @@ class Calculator {
     public function divide($a, $b) {
         return $a / $b;
     }
+    public function add($a, $b) {
+        return $a + $b;
+    }
+
+    public function subtract($a, $b) {
+        return $a - $b;
+    }
+
+    public function multiply($a, $b) {
+        return $a * $b;
+    }
+
+    public function divide($a, $b) {
+        return $a / $b;
+    }
 }
