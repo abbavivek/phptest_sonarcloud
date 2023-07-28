0dccff686a66ed9beef1cd93a0c9920143d1c22a
Abba Vivek <abbaviveks@gmail.com>
2023-07-28 16:19:42 +0800

    test report coverage

--- src/Calculator.php
+++ src/Calculator.php
@@ -9,4 +9,12 @@ class Calculator {
     public function subtract($a, $b) {
         return $a - $b;
     }
+
+    public function multiply($a, $b) {
+        return $a * $b;
+    }
+
+    public function divide($a, $b) {
+        return $a / $b;
+    }
 }
