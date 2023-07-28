8a6cb0f707a762314c64afe41347949331ea9053
Abba Vivek <abbaviveks@gmail.com>
2023-07-28 17:40:43 +0800

    testing duplication

---
+++ Calculator.php
@@ -0,0 +1,35 @@
+<?php
+// Calculator.php
+
+class Calculator {
+    public function add($a, $b) {
+        return $a + $b;
+    }
+
+    public function subtract($a, $b) {
+        return $a - $b;
+    }
+
+    public function multiply($a, $b) {
+        return $a * $b;
+    }
+
+    public function divide($a, $b) {
+        return $a / $b;
+    }
+    public function asqrt($a) {
+        return sqrt($a);
+    }
+
+    public function arand($a, $b) {
+        return rand($a, $b);
+    }
+
+    public function amin($a, $b) {
+        return min($a, $b);
+    }
+
+    public function amax($a, $b) {
+        return max($a, $b);
+    }
+}
